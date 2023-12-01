b4dbdcd5ab6d98dbee8fcaec177535859d9877b8
Green Sky <green@g-s.xyz>
2023-12-01 02:48:18 +0100

    basic main loop running

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+#include <solanaceae/util/simple_config_model.hpp>
+#include <solanaceae/contact/contact_model3.hpp>
+#include <solanaceae/message3/registry_message_model.hpp>
+#include <solanaceae/message3/message_time_sort.hpp>
+#include <solanaceae/plugin/plugin_manager.hpp>
+#include <solanaceae/toxcore/tox_event_logger.hpp>
+#include "./tox_private_impl.hpp"
+
+#include <solanaceae/tox_contacts/tox_contact_model2.hpp>
+#include <solanaceae/tox_messages/tox_message_manager.hpp>
+#include <solanaceae/tox_messages/tox_transfer_manager.hpp>
+
+#include "./tox_client.hpp"
+#include "./auto_dirty.hpp"
+
 #include <chrono>
 #include <thread>
 #include <atomic>
@@ -20,7 +35,6 @@ void sigint_handler(int signo) {
 }
 #endif
 
-
 int main(int argc, char** argv) {
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
 	struct sigaction sigint_action;
@@ -31,16 +45,64 @@ int main(int argc, char** argv) {
 #elif defined (_WIN32)
 	signal(SIGINT, sigint_handler);
 #endif
+	auto last_time = std::chrono::steady_clock::now();
 
 	// TODO: parse arg
 	// totato <config.json> -p <path/to/plugin.so>
 
-	//auto last_time = std::chrono::steady_clock::now();
+	// HACK: config
+	std::string tox_profile_path {"totato.tox"};
+
+	SimpleConfigModel conf;
+	Contact3Registry cr;
+	RegistryMessageModel rmm{cr};
+	MessageTimeSort mts{rmm};
+
+	PluginManager pm;
+
+	ToxEventLogger tel{std::cout};
+	ToxClient tc{tox_profile_path, ""};
+	ToxPrivateImpl tpi{tc.getTox()};
+	AutoDirty ad{tc};
+	ToxContactModel2 tcm{cr, tc, tc};
+	ToxMessageManager tmm{rmm, cr, tcm, tc, tc};
+	ToxTransferManager ttm{rmm, cr, tcm, tc, tc};
+
+	{ // setup plugin instances
+		g_provideInstance<ConfigModelI>("ConfigModelI", "host", &conf);
+		g_provideInstance<Contact3Registry>("Contact3Registry", "host", &cr);
+		g_provideInstance<RegistryMessageModel>("RegistryMessageModel", "host", &rmm);
+
+		//g_provideInstance<ToxI>("ToxI", "host", &tc);
+		//g_provideInstance<ToxPrivateI>("ToxPrivateI", "host", &tpi);
+		//g_provideInstance<ToxEventProviderI>("ToxEventProviderI", "host", &tc);
+		//g_provideInstance<ToxContactModel2>("ToxContactModel2", "host", &tcm);
+
+		// TODO: pm?
+	}
+
+	for (const auto& ppath : {"../../solanaceae_ecosystem/build/bin/libplugin_transfer_auto_accept.so"}) {
+		if (!pm.add(ppath)) {
+			std::cerr << "TOTATO error: loading plugin '" << ppath << "' failed!\n";
+			// thow?
+			assert(false && "failed to load plugin");
+		}
+	}
+
+	conf.dump();
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(10)); // at startup, just to be safe
 
 	while (!quit) {
 		//auto new_time = std::chrono::steady_clock::now();
+
+		quit = !tc.iterate();
+		tcm.iterate(/*time_delta*/0.02f);
+		ttm.iterate();
+
+		mts.iterate();
+
+		pm.tick(/*time_delta*/0.02f);
 
 		//std::this_thread::sleep_for( // time left to get to 60fps
 			//std::chrono::duration<float, std::chrono::seconds::period>(0.0166f) // 60fps frame duration
